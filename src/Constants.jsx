665f22c830d7a8b25e31cd30d1cc5e6ed96a19f4
lejler <layla.shokri@gmx.de>
2023-04-10 21:24:59 +0200

    added constants as external file

---
+++ src/Constants.jsx
@@ -0,0 +1,13 @@
+export const serviceOptions = [
+  { value: "120686", label: "Anmeldung einer Wohnung" },
+  { value: "120703", label: "Personalausweis beantragen" },
+  { value: "324325", label: "Personalausweis abholen" },
+];
+
+export const locationOptions = [
+  { value: "122231", label: "Bürgeramt 1 (Kreuzberg), Yorckstraße" },
+  {
+    value: "327346",
+    label: "Bürgeramt 1 (Kreuzberg), Yorckstraße - Vorzugstermine",
+  },
+];
